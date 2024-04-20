89345469acbfd7ec9101bdb8bfa219a491ca4778
Ashutosh Bhattarai <ashutoshbhattarai90@gmail.com>
2024-04-20 17:34:36 +0545

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC;
+
+public class StudentController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Message"] = "Hello World!";
+        ViewData["PersonName"] = "John Doe";
+        return View();
+    }
+}
